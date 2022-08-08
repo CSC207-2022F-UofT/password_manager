efcf844f7fea2b3b79cd6db80031a7a61b09ed36
pritishmishra <coolpritish.mishra@gmail.com>
2022-08-08 11:01:02 -0400

    Adding CheckPassword button to Strength-Checker dialog box

---
+++ src/UI/Button.java
@@ -0,0 +1,13 @@
+package src.UI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Button {
+    JButton button;
+    public void createButtonWithIcon(JFrame dialogBox, String buttonText, ImageIcon icon) {
+        button = new JButton(buttonText, icon);
+        button.setBounds(90,150,240,30);
+        dialogBox.add(button);
+    }
+}
--- src/controller/UIController.java
+++ src/controller/UIController.java
@@ -1,6 +1,8 @@
 package src.controller;
 
+import src.UI.Button;
 import src.UI.DialogBox;
+import src.UI.Icon;
 import src.UI.Label;
 import src.UI.TextField;
 
@@ -18,5 +20,9 @@ public class UIController {
         label.createLabel(dialogBox.jframe, "Enter your password to check its strength", Color.blue);
         TextField passwordTextField = new TextField();
         passwordTextField.createTextField(dialogBox.jframe);
+        Icon icon = new Icon();
+        icon.createIcon("../resources/icons/password.png");
+        Button checkPasswordButton = new Button();
+        checkPasswordButton.createButtonWithIcon(dialogBox.jframe, "How strong is your password?", icon.icon);
     }
 }
