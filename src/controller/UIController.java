b8e4c11e6734eb8158a5fd5068e39966695677f0
pritishmishra <coolpritish.mishra@gmail.com>
2022-08-09 10:51:16 -0400

    Refactoring UIController to add new controller for strength-checker

--- src/controller/UIController.java
+++ src/controller/UIController.java
@@ -7,22 +7,15 @@ import src.UI.Label;
 import src.UI.TextField;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class UIController {
+public class UIController implements ActionListener {
     public void createPasswordGeneratorBox() {
 
     }
 
-    public void createStrengthCheckerBox() {
-        DialogBox dialogBox = new DialogBox();
-        dialogBox.createDialogBox("Strength Checker", 400, 360);
-        Label label = new Label();
-        label.createLabel(dialogBox.jframe, "Enter your password to check its strength", Color.blue);
-        TextField passwordTextField = new TextField();
-        passwordTextField.createTextField(dialogBox.jframe);
-        Icon icon = new Icon();
-        icon.createIcon("../resources/icons/password.png");
-        Button checkPasswordButton = new Button();
-        checkPasswordButton.createButtonWithIcon(dialogBox.jframe, "How strong is your password?", icon.icon);
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
 }
